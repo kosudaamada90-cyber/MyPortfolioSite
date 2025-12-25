e2e3200076d8b101c5136b86166c66e926a0e428
kosudaamada90-cyber <kosudaamada90@gmail.com>
2025-12-25 16:46:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('section').forEach(section => {
+  section.classList.add('hidden');
+  observer.observe(section);
+});
+
+
+
+
+
+
+
+
